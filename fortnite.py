ab35805bdc1b04f9dc3ee314b949994fd8e57be6
kaaax0815 <32197462+kaaax0815@users.noreply.github.com>
2020-04-15 23:00:45 +0200

    Add files via upload

--- fortnite.py
+++ fortnite.py
@@ -50,10 +50,10 @@ try:
     import psutil
 
 except ModuleNotFoundError as e:
-    ##print(e)
-    ##print('Failed to import 1 or more modules, running "INSTALL PACKAGES.bat"'
+    print(e)
+    print('Failed to import 1 or more modules, running "INSTALL PACKAGES.bat"'
           'might fix the issue, if not please create an issue or join'
-          'the support server.')
+         'the support server.')
     sys.exit()
 
 # Imports uvloop and uses it if installed (Unix only).
