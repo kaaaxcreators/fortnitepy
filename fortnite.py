ed6cb3192e9af50ca814ec70b1bf760b2e770f30
kaaax0815 <32197462+kaaax0815@users.noreply.github.com>
2020-07-07 16:57:00 +0200

    set new loadout on party member join

--- fortnite.py
+++ fortnite.py
@@ -276,6 +276,21 @@ async def event_friend_request(request: fortnitepy.PendingFriend) -> None:
 @client.event
 async def event_party_member_join(member: fortnitepy.PartyMember) -> None:
     await ctx.send(f"Creator-Code: schokobanane-btw")
+    await client.party.me.set_battlepass_info(
+        has_purchased=True,
+        level=105,
+    )
+    await client.party.me.set_banner(
+        season_level=105
+    )
+    await client.party.me.set_outfit(
+        asset='CID_028_ATHENA_COMMANDO_F'
+    )
+    await client.party.me.clear_emote()
+
+    await client.party.me.set_emote(
+        asset='EID_HAPPYWAVE'
+    )
 
 
 @client.event
